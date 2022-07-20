6a5d7a8312439ab30c0d7372c4975b458ec00490
wangchaolong <wangchaolong@mastercom.cn>
2022-07-20 17:34:35 +0800

    更新代码

--- OCRunner_Use/AppDelegate.m
+++ OCRunner_Use/AppDelegate.m
@@ -20,28 +20,33 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-#if 1
+#if 0
     NSString *patchFilePath = [[NSBundle mainBundle] pathForResource:@"binarypatch" ofType:nil];
     [ORInterpreter excuteBinaryPatchFile:patchFilePath];
     [self showVC];
 #else
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-    NSURL *URL = [NSURL URLWithString:@"https://github.com/Charles2016/binarypatch/raw/main/binarypatch_Use.zip"];
+    NSURL *URL = [NSURL URLWithString:@"https://github.com/Charles2016/OCRunner_Use/raw/master/OCRunner_Use/binarypatch_Use.zip"];
     NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-       NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
-       return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
     } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
-       NSLog(@"File downloaded to: %@", filePath);
-       NSString *filePathStr = filePath.relativePath;
-       NSString *toPath = [filePathStr stringByDeletingLastPathComponent];
-       [SSZipArchive unzipFileAtPath:filePathStr toDestination:toPath progressHandler:nil completionHandler:^(NSString *path, BOOL succeeded, NSError *error) {
-           NSString *finalPath = [NSString stringWithFormat:@"%@/binarypatch", toPath];
+        NSLog(@"File downloaded to: %@", filePath);
+        NSString *filePathStr = filePath.relativePath;
+        NSString *toPath = [filePathStr stringByDeletingLastPathComponent];
+        NSString *finalPath = [NSString stringWithFormat:@"%@/binarypatch", toPath];
+        //先移除已存在的解压包，以防解压过后的文件干扰到，取不到最新的二进制文件
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if ([fileManager fileExistsAtPath:finalPath]) {
+            [fileManager removeItemAtPath:finalPath error:nil];
+        }
+        [SSZipArchive unzipFileAtPath:filePathStr toDestination:toPath progressHandler:nil completionHandler:^(NSString *path, BOOL succeeded, NSError *error) {
            NSLog(@"File unzip to: %@", finalPath);
            [ORInterpreter excuteBinaryPatchFile:finalPath];
            [self showVC];
-       }];
+        }];
     }];
     [downloadTask resume];
 #endif
